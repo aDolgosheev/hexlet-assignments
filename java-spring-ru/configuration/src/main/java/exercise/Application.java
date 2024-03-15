493686cf738b831b187a3df1a8619b6433eb7ddd
@hexlet/cli <support@hexlet.io>
2024-03-15 17:15:53 +0300

    submit java-spring-ru/configuration

--- java-spring-ru/configuration/src/main/java/exercise/Application.java
+++ java-spring-ru/configuration/src/main/java/exercise/Application.java
@@ -1,21 +1,16 @@
 package exercise;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Value;
+import exercise.component.UserProperties;
+import exercise.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import  org.springframework.beans.factory.annotation.Autowired;
 
-import exercise.model.User;
-import exercise.component.UserProperties;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 @RestController
@@ -26,25 +21,21 @@ public class Application {
 
     // BEGIN
 
-    @Component
-    @ConfigurationProperties(prefix = "users.admins")
-    public static class AdminsEmails {
-        static List<String> admins;
-    }
+    @Autowired
+    private UserProperties usersInfo;
 
     @GetMapping("/admins")
-//    @ConfigurationProperties(prefix = "users.admins")
-    public List<User> adminsShow() {
-        List<User> tempList = new ArrayList<>();
-        for (User user : users) {
-            for (String i : AdminsEmails.admins) {
-                if (user.getEmail().equals(i)) {
-                    tempList.add(user);
-                }
-            }
-        }
-        return tempList.stream().sorted().toList();
+    public List<String> getAdmins() {
+
+        List<String> adminEmails = usersInfo.getAdmins();
+
+        return users.stream()
+                .filter(u -> adminEmails.contains(u.getEmail()))
+                .map(u -> u.getName())
+                .sorted()
+                .toList();
     }
+
     // END
 
     @GetMapping("/users")
@@ -55,8 +46,8 @@ public class Application {
     @GetMapping("/users/{id}")
     public Optional<User> show(@PathVariable Long id) {
         var user = users.stream()
-            .filter(u -> u.getId() == id)
-            .findFirst();
+                .filter(u -> u.getId() == id)
+                .findFirst();
         return user;
     }
 
