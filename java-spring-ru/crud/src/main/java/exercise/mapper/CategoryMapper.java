6fc918804486fac9d943ea17419fc3a37d23197e
@hexlet/cli <support@hexlet.io>
2024-04-08 12:42:36 +0300

    submit java-spring-ru/crud

---
+++ java-spring-ru/crud/src/main/java/exercise/mapper/CategoryMapper.java
@@ -0,0 +1,23 @@
+package exercise.mapper;
+
+import exercise.dto.*;
+import exercise.model.Category;
+import exercise.model.Product;
+import org.mapstruct.*;
+
+// BEGIN
+
+@Mapper(
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class CategoryMapper {
+
+    public abstract Category map(CategoryCreateDTO dto);
+
+    public abstract CategoryDTO map(Category model);
+
+}
+
+// END
