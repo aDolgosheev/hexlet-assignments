ccfaf9dbc7902fc5867b284035af60f2e01f6159
@hexlet/cli <support@hexlet.io>
2024-05-06 15:17:12 +0300

    submit java-oop-ru/interfaces

--- java-oop-ru/interfaces/src/main/java/exercise/App.java
+++ java-oop-ru/interfaces/src/main/java/exercise/App.java
@@ -8,25 +8,12 @@ import java.util.stream.Collectors;
 
 public class App {
 
-    public static List<String> buildApartmentsList(List<Home> apartmentList, int count) {
-        apartmentList.sort(Home::compareTo);
-        List<String> apartmentCountListToString = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            apartmentCountListToString.add(apartmentList.get(i).toString());
-        }
-        return apartmentCountListToString;
-    }
-
-    public static void main(String[] args) {
-        List<Home> apartments = new ArrayList<>(List.of(
-                new Flat(41, 3, 10),
-                new Cottage(125.5, 2),
-                new Flat(80, 10, 2),
-                new Cottage(150, 3)
-        ));
-
-        List<String> result = App.buildApartmentsList(apartments, 3);
-        System.out.println(result);
+    public static List<String> buildApartmentsList(List<Home> apartments, int count) {
+        return apartments.stream()
+                .sorted(Home::compareTo)
+                .limit(count)
+                .map(Home::toString)
+                .toList();
     }
 }
 
